Fix batch computation.

modules/diffusion/tensorrt/pwp.py
@@ -190,6 +190,7 @@ class TensorRTPromptWeightingPipeline:
         text_input_ids = torch.tensor(
             prompt_tokens, dtype=torch.int32, device=self.device
         )
+        text_input_ids = torch.cat([text_input_ids] * batch_size)
 
         uncond_tokens, uncond_weights = pad_tokens_and_weights(
             uncond_tokens,
@@ -201,6 +202,7 @@ class TensorRTPromptWeightingPipeline:
         uncond_input_ids = torch.tensor(
             uncond_tokens, dtype=torch.int32, device=self.device
         )
+        uncond_input_ids = torch.cat([uncond_input_ids] * batch_size)
 
         text_input_ids_inp = cuda.DeviceView(
             ptr=text_input_ids.data_ptr(), shape=text_input_ids.shape, dtype=np.int32
@@ -210,9 +212,9 @@ class TensorRTPromptWeightingPipeline:
         )["text_embeddings"]
 
         seq_len = text_embeddings.shape[1]
-        text_embeddings = text_embeddings.repeat(1, num_images_per_prompt, 1)
+        text_embeddings = text_embeddings.repeat(1, 1, 1)
         text_embeddings = text_embeddings.view(
-            batch_size * num_images_per_prompt, seq_len, -1
+            1, seq_len, -1
         )
 
         max_length = text_input_ids.shape[-1]
@@ -227,9 +229,9 @@ class TensorRTPromptWeightingPipeline:
         )["text_embeddings"]
 
         seq_len = uncond_embeddings.shape[1]
-        uncond_embeddings = uncond_embeddings.repeat(1, num_images_per_prompt, 1)
+        uncond_embeddings = uncond_embeddings.repeat(1, 1, 1)
         uncond_embeddings = uncond_embeddings.view(
-            batch_size * num_images_per_prompt, seq_len, -1
+            1, seq_len, -1
         )
 
         prompt_weights = torch.tensor(

modules/diffusion/tensorrt/runner.py
@@ -120,7 +120,6 @@ class TensorRTDiffusionRunner(BaseRunner):
 
     def loadResources(self, image_height, image_width, batch_size, seed):
         self.generator = torch.Generator(device="cuda").manual_seed(seed)
-
         # Allocate buffers for TensorRT engine bindings
         for model_name, obj in self.models.items():
             self.engines[model_name].allocate_buffers(shape_dict=obj.get_shape_dict(batch_size, image_height, image_width), device=self.device)
@@ -223,7 +222,7 @@ class TensorRTDiffusionRunner(BaseRunner):
         images = self.run_engine("vae", {"latent": sample_inp})["images"]
         return images
 
-    def to_image(self, images, upscale=True):
+    def to_image(self, images, upscale=False):
         images = (
             ((images + 1) * 255 / 2)
             .clamp(0, 255)
@@ -275,10 +274,8 @@ class TensorRTDiffusionRunner(BaseRunner):
 
         # Start pipeline
         for i in range(opts.batch_count):
-            batch_size = 1
-            manual_seed = opts.seed + i
-
-            self.loadResources(opts.image_height, opts.image_width, batch_size, manual_seed)
+            manual_seed = opts.seed * i
+            self.loadResources(opts.image_height, opts.image_width, opts.batch_size, manual_seed)
 
             with torch.inference_mode(), torch.autocast("cuda"), trt.Runtime(
                 TRT_LOGGER